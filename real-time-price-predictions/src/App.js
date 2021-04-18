9b00a3bd0164fd4c328c989fe7f2aa0a42bd1685
alacwong <mr6902spades@gmail.com>
2021-04-18 13:32:29 -0400

    fix images

--- real-time-price-predictions/src/App.js
+++ real-time-price-predictions/src/App.js
@@ -14,6 +14,15 @@ import Col from "react-bootstrap/Col";
 import HomePage from "./component/homepage.js";
 import ErrorPage from "./component/errorpage.js";
 
+const images = [
+    'https://ibb.co/YZTvD70',
+    'https://ibb.co/XJd0FtV',
+'https://ibb.co/kxG1rSk',
+    'https://ibb.co/KbbHfYM',
+        'https://ibb.co/Fqh30Ky',
+            'https://ibb.co/dj3Vtmk',
+                'https://ibb.co/sFMKL2W',
+]
 
 
 const App = () => (
@@ -33,14 +42,15 @@ const Main = () => (
 );
 
 const Home = () => (
+
   <div className="home center">
     <Container>
       <Row>
         <Col>
-          <HomePage></HomePage>
+          <HomePage/>
         </Col>
         <Col>
-          <img src={"./assets/Cryto_the_Clam.png"} alt="main-img" class="mainpage-img center" />
+          <img src={'assets/Crypto_the_Clam.png'} alt="main-img" class="mainpage-img center" />
         </Col>
       </Row>
     </Container>
--- real-time-price-predictions/src/component/errorpage.js
+++ real-time-price-predictions/src/component/errorpage.js
@@ -1,19 +1,15 @@
-import right from "../assets/right.png";
-import left from "../assets/left.png";
-import errorcode from "../assets/404.png";
-import text from "../assets/404text.png";
 import Col from "react-bootstrap/Col";
 
 const ErrorPage = () => {
   return (
     <div>
-      <img src={left} class="left" />
-      <img src={right} class="right" />
+      <img src={'assets/left.png'} class="left" />
+      <img src={'assets/right.png'} class="right" />
       <div className="center" style={{ "text-align": "center" }}>
         <div className="center">
           <Col>
-            <img src={errorcode} style={{ margin: "20px" }} />
-            <img src={text} />
+            <img src={'assets/404.png'} style={{ margin: "20px" }} />
+            <img src={'404text.png'} />
           </Col>
         </div>
       </div>
